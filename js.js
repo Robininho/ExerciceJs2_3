4e0a6da7f44e69e383a5879ea6bd81895553dced
Robininho <robin.halberda@orange.fr>
2018-10-10 14:54:18 +0200

    Finish

--- js.js
+++ js.js
@@ -1,5 +1,3 @@
-var message = document.getElementById("lastname");
-
-message.addEventListener("blur", function() {
-  alert("Merci de votre participation");
+lastname.addEventListener("keyup", function() {
+  alert(lastname.value);
 });
